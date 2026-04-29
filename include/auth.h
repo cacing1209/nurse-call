6e5c35b9a67429739196aad430805852c4bc3d40
diki lmj <dikilmj@gmail.com>
2026-04-30 01:16:12 +0700

    in-test

--- include/auth.h
+++ include/auth.h
@@ -1,11 +1,11 @@
 #ifndef AUTH_H
 #include <Arduino.h>
 #include <string.h>
-#include <primary_auth.h>
+// #include <primary_auth.h>
 struct auth_state
 {
 private:
-    const char token[70] = token_sck; 
+    const char token[70] = "token_sck"; 
 public:
     bool check_auth(String auth_h = "xxx");
 };
